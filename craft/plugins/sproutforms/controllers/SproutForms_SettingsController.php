9357b975f883af8c0514522f34a2195bca188bbf
Josh Angell <josh@supercooldesign.co.uk>
2014-11-14 15:56:05 +0000

    ok new forms and a way to fix it now!

--- craft/plugins/sproutforms/controllers/SproutForms_SettingsController.php
+++ craft/plugins/sproutforms/controllers/SproutForms_SettingsController.php
@@ -3,85 +3,55 @@ namespace Craft;
 
 class SproutForms_SettingsController extends BaseController
 {
+	/**
+	 * Save Settings to the Database
+	 *
+	 * @return mixed Return to Page
+	 */
+	public function actionSettingsIndexTemplate()
+	{
+		$settingsModel = new SproutForms_SettingsModel;
+
+		$settings = craft()->db->createCommand()
+			->select('settings')
+			->from('plugins')
+			->where('class=:class', array(':class'=> 'SproutForms'))
+			->queryScalar();
+
+		$settings = JsonHelper::decode($settings);
+		$settingsModel->setAttributes($settings);
+
+		$variables['settings'] = $settingsModel;
+
+		// Load our template
+		$this->renderTemplate('sproutforms/settings', $variables);
+
+	}
 
 	/**
-	 * Save settings
+	 * Save Plugin Settings
 	 * 
 	 * @return void
 	 */
-	public function actionSaveForm() {
-        
-        $this->requirePostRequest();
+	public function actionSaveSettings()
+	{
+		$this->requirePostRequest();
+		$settings = craft()->request->getPost('settings');
 
-        $form = new SproutForms_FormModel;
-        
-        // Set our variables
-        $form->name     = craft()->request->getPost('formName');
-        $form->handle   = craft()->request->getPost('formHandle');
+		if (craft()->sproutForms_settings->saveSettings($settings))
+		{
+			craft()->userSession->setNotice(Craft::t('Settings saved.'));
 
-        // If we have an id, update the existing form, otherwise, lets create a new form
-        if ($form->id)
-        {
-            // $entryRecord = EntryRecord::model()->with('element', 'entryTagEntries')->findById($entry->id);
+			$this->redirectToPostedUrl();
+		}
+		else
+		{
+			craft()->userSession->setError(Craft::t('Couldn’t save settings.'));
 
-            // if (!$entryRecord)
-            // {
-            //     throw new Exception(Craft::t('No entry exists with the ID “{id}”', array('id' => $entry->id)));
-            // }
-
-            // $elementRecord = $entryRecord->element;
-        }
-        else
-        {
-            $formRecord = new SproutForms_FormRecord();
-
-            // @TODO - Support Form as an ElementType
-            // $elementRecord = new ElementRecord();
-            // $elementRecord->type = ElementType::Form;
-        }
-
-        $formRecord->name       = $form->name;
-        $formRecord->handle     = $form->handle;
-        
-        $formRecord->validate();
-        $form->addErrors($formRecord->getErrors());
-
-
-        if (!$form->hasErrors())
-        {               
-            craft()->db->createCommand()
-                ->insert('sproutforms_forms', $form->getAttributes());
-
-        	craft()->userSession->setNotice(Craft::t('Form settings saved.'));
-   			$this->redirectToPostedUrl();
-        }
-        else
-        {
-            craft()->userSession->setError(Craft::t('Couldn’t save form settings.'));
-        	craft()->urlManager->setRouteVariables(array(
-                'form' => $form
-            ));
-        }
-		
-        // SproutForms_FormModel::populateModel($form);
-
-        // $form['name'] = craft()->request->getPost('formName');
-        // $form['handle'] = craft()->request->getPost('formHandle');
-
-        
-
-        // craft()->db->createCommand()->insert(
-        //     'sproutforms_forms',
-        //     array(
-        //         'name' 		=> craft()->request->getPost('formName'), 
-        //         'handle'	=> craft()->request->getPost('formHandle')
-        //     ),
-        //     'name=:name',
-        //     array(
-        //         ':name'=>'MoreInfo'
-        //     )
-        // );
-
-    }
-
+			// Send the settings back to the template
+			craft()->urlManager->setRouteVariables(array(
+				'settings' => $settings
+			));
+		}
+	}
 }
