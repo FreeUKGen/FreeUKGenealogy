8dccfd25031dbfc3e8adafbe4a376ee01b09464e
Josh Angell <josh@supercooldesign.co.uk>
2014-11-18 09:30:35 +0000

    only parsley if we have a form

--- public/scripts/src/app.js
+++ public/scripts/src/app.js
@@ -115,26 +115,29 @@
   /**
    * Article form validaton
    */
-  $('.article__form form label.required').each(function(){
-    var $field = $(this).parents('.heading').parents('.field'),
-        id = $field.attr('id');
+  if ( $('.article__form form').length )
+  {
+    $('.article__form form label.required').each(function(){
+      var $field = $(this).parents('.heading').parents('.field'),
+          id = $field.attr('id');
 
-    $(this).parents('.heading').next('.input').find('input, select, textarea').prop('required', true);
+      $(this).parents('.heading').next('.input').find('input, select, textarea').prop('required', true);
 
-    if ( $field.hasClass('checkboxes') || $field.hasClass('radiobuttons') )
-    {
-      $field.find('.input').append('<div id="errors-'+id+'"/>');
-      $field.find('input').attr('data-parsley-errors-container', '#errors-'+id);
-
-      if ( $field.find('input').length > 1 )
+      if ( $field.hasClass('checkboxes') || $field.hasClass('radiobuttons') )
       {
-        $field.find('input').attr('data-parsley-required-message', 'Please select at least one option.');
-      }
-    }
+        $field.find('.input').append('<div id="errors-'+id+'"/>');
+        $field.find('input').attr('data-parsley-errors-container', '#errors-'+id);
 
-  }).promise().done(function(){
-    $('.article__form form').parsley();
-  });
+        if ( $field.find('input').length > 1 )
+        {
+          $field.find('input').attr('data-parsley-required-message', 'Please select at least one option.');
+        }
+      }
+
+    }).promise().done(function(){
+      $('.article__form form').parsley();
+    });
+  }
 
 
 })();
